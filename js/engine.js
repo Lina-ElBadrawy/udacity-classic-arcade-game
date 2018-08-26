2f832b4a9960d51a6a6bf8b5f281fb04c19c396a
Lina <40521260+Lina-ElBadrawy@users.noreply.github.com>
2018-08-26 12:34:15 -0400

    inheritence refactor

--- js/engine.js
+++ js/engine.js
@@ -182,4 +182,25 @@ var Engine = (function(global) {
      * from within their app.js files.
      */
     global.ctx = ctx;
+    debugger;
+    function getCanvesCoordinates(){
+        return {
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top
+          };
+        
+        var canvas = document.getElementById('myCanvas');
+        var context = canvas.getContext('2d');
+  
+        canvas.addEventListener('mousemove', function(evt) {
+          var mousePos = getMousePos(canvas, evt);
+          var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
+          writeMessage(canvas, message);
+        }, false);
+   
+        
+    }
+    ctx.onmouseover=function(x,y){
+        debugger;
+    }
 })(this);
